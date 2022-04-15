d94bd3bc02feac3cc0b2c1a3d44d7d5778bae0e2
Michelle Flores <lmfg2456@gmail.com>
2022-04-15 09:17:11 -0700

    created models/migrations

---
+++ migrations/20220415160349-create-anime.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('animes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      releaseDate: {
+        type: Sequelize.STRING
+      },
+      episodes: {
+        type: Sequelize.INTEGER
+      },
+      seasons: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      watcherId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('animes');
+  }
+};
