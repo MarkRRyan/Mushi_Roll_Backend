4a8d11e2556819a9379786cbd0026dbc7f031c30
Michelle Flores <lmfg2456@gmail.com>
2022-04-15 23:56:31 -0700

    commented out Create method for watchlist

--- controllers/WatchlistController.js
+++ controllers/WatchlistController.js
@@ -1,11 +1,14 @@
+const req = require('express/lib/request')
+const res = require('express/lib/response')
 const { User, Anime} = require('../models')
 
 const GetLists = async (req, res) => {
+  //http://localhost:3001/lists/allLists
     try {
       const list = await User.findAll({
         include: [
           {
-            model: Anime,
+            model: Anime, 
             as: 'watch_list',
             through: { attributes: [] }
           }
@@ -19,16 +22,54 @@ const GetLists = async (req, res) => {
 
 
 
-const createWatchlist = async () => {
-    try {
-      
-    } catch (error) {
-      
-      console.log(error)
-    }
-  }
+// const CreateWatchlist = async (req, res) => {
+//     try {
+//         // let User = User.body
+//         // let Anime = Anime.body 
+//         let newWatchlist = await User.create({
+//           include: [{  
+//               model: Anime,
+//               as: 'watch_list',
+//               through: { attributes: [] }
+//             }
+//           ]
+//         })
+//         res.json(newWatchlist)
+//       } catch (error) {
+//       throw(error)
+//     }
+//   }
+
+
+
+// const CreateWatchlist = async (req, res) => {
+//   try {
+//      let userId = req.params.userId
+//      let animeId = req.params.animeId
+//      let watchlist = {
+//        userId, animeId,
+//        ...req.body
+//      }
+//      let newWatchlist = await User.create(watchlist
+//       , {
+//       include:[
+//         {
+//           model: Anime, 
+//           as: 'watch_list',
+//           through: { attributes: [] }
+//         }
+//       ] }
+//       )
+//      res.json(newWatchlist)
+//     } catch (error) {
+//     throw(error)
+//   }
+// }
+
+
+    
 
   module.exports = {
       GetLists,
-    //   CreateWatchlist
+      CreateWatchlist
   }
