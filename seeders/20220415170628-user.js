c5e6705a5c24e562c1430166c2bee40274a470c7
Mark Ryan <markrryan99@gmail.com>
2022-04-20 07:06:52 -0700

    slight issue dont worry about it

--- seeders/20220415170628-user.js
+++ seeders/20220415170628-user.js
@@ -4,7 +4,7 @@ const falso = require('@ngneat/falso')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const users = [...Array(50)].map((_) => ({
+    const users = [...Array(5)].map((_) => ({
       username: falso.randUserName(),
       email: falso.randEmail(),
       passwordDigest: falso.randPassword(),
