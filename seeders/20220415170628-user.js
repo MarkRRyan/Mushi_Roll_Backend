a81277c522341448fdfd8d0ef8060bb768bcc8a1
Nell Flores <nell.djmf@gmail.com>
2022-04-21 17:53:16 -0400

    set seed to 1 user to prevent empty watchlists

--- seeders/20220415170628-user.js
+++ seeders/20220415170628-user.js
@@ -4,8 +4,8 @@ const falso = require('@ngneat/falso')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const users = [...Array(5)].map((_) => ({
-      username: falso.randUserName(),
+    const users = [...Array(1)].map((_) => ({
+      username: "super_weeb",
       email: falso.randEmail(),
       passwordDigest: falso.randPassword(),
       createdAt: new Date(),
